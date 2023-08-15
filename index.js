67a7bcc595fad3d4e4536510f15dbc3883400bb6
MasukBD <imahfuz2020@outlook.com>
2023-08-16 00:50:53 +0600

    get data

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-
+        const serviceCollection = client.db('CarHubDB').collection('services');
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
