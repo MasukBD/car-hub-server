cd4d0181571f960d32966dfe65efc6976e8d87d6
MasukBD <imahfuz2020@outlook.com>
2023-08-21 10:50:31 +0600

    jwt verify

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    };
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -69,7 +85,13 @@ async function run() {
         // Order Handle Area 
 
         // get specific data by logged in user
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+
+            if (decoded.email != req.query.email) {
+                return res.send({ error: 1, message: 'No Access' })
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
