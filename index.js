20ee00fd878b038107ce69f12d37c8d4364f3f8a
MasukBD <imahfuz2020@outlook.com>
2023-10-28 21:46:32 +0600

    service sorted by price

--- index.js
+++ index.js
@@ -66,7 +66,11 @@ async function run() {
         // Service Area 
 
         app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find();
+            const query = {};
+            const options = {
+                sort: { "price": 1 }
+            }
+            const cursor = serviceCollection.find(query, options);
             const result = await cursor.toArray();
             res.send(result);
         });
